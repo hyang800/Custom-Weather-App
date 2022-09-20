bf7b72a136cf4ec33da22d5f9158715160bff539
Hao Yang <hyang800@usc.edu>
2022-09-20 12:29:51 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Search from './Components/Search'
 
 function App() {
-  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=57a7da7c26da29c1d15b4c8e80f6276c'
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=xxxxxxxxxxxxxx'
   return (
     <div className='App'>
       <Search />
